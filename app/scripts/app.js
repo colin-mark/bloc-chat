297f21ceb3d74de621b07408d28822c102344b8d
colin-mark <colin.alcorn@gmail.com>
2017-10-11 22:38:38 -0500

    Checkpoint 2 List Chat Rooms: completed

--- app/scripts/app.js
+++ app/scripts/app.js
@@ -7,10 +7,10 @@
              });
 
         $stateProvider
-            .state('home', {
+            .state('room', {
                 url: '/',
-                controller: 'HomeCtrl as home',
-                templateUrl: '/templates/home.html'
+                controller: 'RoomCtrl as room',
+                templateUrl: '/templates/room.html'
             });
     }
 
